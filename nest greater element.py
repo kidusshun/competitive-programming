834a1ac34992f6b94cb324e03a2b09f2f65d4f12
unknown <kidusshun@gmail.com>
2023-02-13 17:57:06 +0300

    solutions

---
+++ nest greater element.py
@@ -0,0 +1,17 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1_index={n:i for i, n in enumerate((nums1))}
+        result= [-1] * len(nums1)
+        stack=[]
+
+        for i in range(len(nums2)):
+            current=nums2[i]
+            while stack and current > stack[-1]:
+                value = stack.pop()
+                idx = nums1_index[value]
+                result[idx]=current
+            if current in nums1:
+                stack.append(current)
+        return result
+            
+nextGreaterElement([4,1,2], [1,3,4,2])
