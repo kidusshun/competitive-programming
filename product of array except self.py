28d181f90c10e71836bd0bda7f9e32263efe0f24
unknown <kidusshun@gmail.com>
2023-02-16 16:50:11 +0300

    improved solution

--- product of array except self.py
+++ product of array except self.py
@@ -1,24 +1,12 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        ans=[]
-        pref_prod=None
-        num_zero=0
-        for num in nums:
-            if num !=0 and pref_prod==None:
-                pref_prod=num
-            elif num!=0:
-                pref_prod*=num
-            elif num==0:
-                num_zero+=1
-        if num_zero>1:
-            ans=[0]*len(nums)
-        elif num_zero==1:
-            for num in nums:
-                if num !=0:
-                    ans.append(0)
-                else:
-                    ans.append(pref_prod)
-        else:
-            for num in nums:
-                ans.append(pref_prod//num)
+        ans=[1]*len(nums)
+        curr_prod=1
+        for i in range(1,len(nums)):
+            curr_prod=curr_prod*nums[i-1]
+            ans[i]=curr_prod
+        curr_prod=1
+        for i in range(len(nums)-2,-1,-1):
+            curr_prod=curr_prod * nums[i+1]
+            ans[i]=curr_prod*ans[i]
         return ans
