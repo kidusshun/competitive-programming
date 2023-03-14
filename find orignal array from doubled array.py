9384c46125609855e8c30ec1f21d8b637e963e6a
unknown <kidusshun@gmail.com>
2023-03-14 20:02:15 +0300

    solution

--- find orignal array from doubled array.py
+++ find orignal array from doubled array.py
@@ -1,5 +1,18 @@
-changed=[1,3,4,2,6,8]
-for num in changed:
-    if num*2 in changed and num%2 == 0 and num//2 not in changed:
-        lst.append(num)
-    print(lst)
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        if len(changed)%2 != 0:
+            return []
+        count=collections.Counter(changed)
+        print(count)
+        changed.sort()
+        lst=[]
+        for num in changed:
+            if num==0 and count[num]>1:
+                count[num]-=1
+                lst.append(num)
+            elif num > 0 and count[num] and count[num*2]:
+                count[num]-=1
+                count[num*2]-=1
+                lst.append(num)
+        print(lst)
+        return lst if len(lst) == len(changed)//2 else []
