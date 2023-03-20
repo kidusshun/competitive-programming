7c09c6c1b43df561a777c5bb324fa1bea8076a78
unknown <kidusshun@gmail.com>
2023-03-20 14:04:11 +0300

    solutions

--- top k frequent elements.py
+++ top k frequent elements.py
@@ -1,12 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count=collections.Counter(nums)
-        values=list(count.values())
-        values.sort()
+        freq=[[] for i in range(len(nums)+1)]
+        for n,c in count.items():
+            freq[c].append(n)
         ans=[]
-        my_set = set(nums)
-        nums = list(my_set)
-        for num in nums:
-            if values.index(count[num])>=(len(values)-k):
-                ans.append(num)
-        return ans
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                ans.append(n)
+                if len(ans)==k:
+                    return ans
