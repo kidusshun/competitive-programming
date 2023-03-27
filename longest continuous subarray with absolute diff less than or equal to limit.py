9c2461b9cf839407469e99f414c20c26d0e9c2ff
unknown <kidusshun@gmail.com>
2023-03-27 19:55:41 +0300

    solution

---
+++ longest continuous subarray with absolute diff less than or equal to limit.py
@@ -0,0 +1,15 @@
+def longestSubarray(nums: list[int], limit: int) -> int:
+        l,r=0,0
+        max_=nums[0]
+        min_=nums[0]
+        while r<len(nums):
+            if nums[r]>max_:max_=nums[r]
+            elif nums[l]<min_:min_=nums[l]
+            if abs(max_-min_)<=limit:
+                r+=1
+            elif abs(max_-min_)>limit:
+                if max_==nums[l]:
+                    max_=nums[l+1]
+                l+=1
+        return r-l
+longestSubarray([8,2,4,7], 4)
