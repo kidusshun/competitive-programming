56c130808a0003ecf03c3aa70a1bd1e3f4bcac2d
unknown <kidusshun@gmail.com>
2023-03-28 20:34:34 +0300

    solutions

--- h-index.py
+++ h-index.py
@@ -1,8 +1,10 @@
-def hIndex(citations: list[int]) -> int:
-    citations=sorted(citations,reverse=True)
-    counter=0
-    for i,j in enumerate(citations):
-        if i<j:
-            counter +=1
-    return counter
-hIndex([100])
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations=sorted(citations,reverse=True)
+        counter=0
+        for i,j in enumerate(citations):
+            if i<j:
+                counter +=1
+            else:
+                break
+        return counter
