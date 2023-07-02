e1205341c3956cb653f441eb0a4837fc482b7ca9
unknown <kidusshun@gmail.com>
2023-07-02 07:34:59 +0300

    solutions

--- Predict the Winner.py
+++ Predict the Winner.py
@@ -1,12 +1,10 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        memo ={}
 
         def score(left,right):
             if left > right:
                 return 0
-            if (left, right) in memo:
-                return memo[i,j]
+
 
             score_left = nums[left] - score(left + 1, right)
             score_right = nums[right] - score(left, right - 1)
