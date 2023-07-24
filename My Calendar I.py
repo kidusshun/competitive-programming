42df7863fa5d8b3d3183cd0c08081ec711c18662
unknown <kidusshun@gmail.com>
2023-07-24 12:25:05 +0300

    solutions

--- My Calendar I.py
+++ My Calendar I.py
@@ -9,9 +9,9 @@ class MyCalendar:
             return True
         else:
             arr = list(self.calendar.keys())
-            result = self.binary(arr, start)
+            ind = self.binary(arr, start)
 
-            if result <= start < self.calendar[result]:
+            if arr[ind] <= start < self.calendar[arr[ind]] or arr[ind+1] <= end <= self.calendar[arr[ind+1]] :
                 return False
             else:
                 self.calendar[start] = end
@@ -21,7 +21,6 @@ class MyCalendar:
     
     def binary(self, arr, start):
         l,r = 0, len(arr) - 1
-        print(arr)
 
         while l<=r:
             mid = (l+r)//2
@@ -31,21 +30,22 @@ class MyCalendar:
                 r= mid - 1
             else:
                 return arr[mid]
-        return arr[mid - 1]
+        if mid == 0:return mid
+        return mid - 1
         
 
 s = MyCalendar()
-s.book(47, 50)
-s.book(33, 41)
-s.book(39, 45)
-s.book(33, 42)
-s.book(25, 32)
-s.book(26, 35)
-s.book(19, 25)
-s.book(3, 8)
-s.book(8, 13)
-s.book(18, 27)
+print(s.book(47, 50))
+print(s.book(33, 41))
+print(s.book(39, 45))
+print(s.book(33, 42))
+print(s.book(25, 32))
+print(s.book(26, 35))
+print(s.book(19, 25))
+print(s.book(3, 8))
+print(s.book(8, 13))
+print(s.book(18, 27))
 [[],[47,50],[33,41],[39,45],[33,42],[25,32],[26,35],[19,25],[3,8],[8,13],[18,27]]
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
-# param_1 = obj.book(start,end)
+# param_1 = print(obj.book(start,end))
