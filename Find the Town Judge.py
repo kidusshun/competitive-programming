19293e2101de2290e759e91876fae7372f9e9948
unknown <kidusshun@gmail.com>
2023-08-21 13:41:54 +0300

    solutions

--- Find the Town Judge.py
+++ Find the Town Judge.py
@@ -1,14 +1,19 @@
 class Solution:
+    def trusted(self, adj, i):
+        for j, lst in adj.items():
+            if i!=j and i not in lst:
+                return False
+        return True
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        incoming = [0 for _ in range(n)]
-        outgoing = [0 for _ in range(n)]
-        outgoing.e
-        for edge in trust:
-            outgoing[edge[0] - 1] +=1
-            incoming[edge[1] - 1] +=1
-        town_judge = -1
-        for i in range(n):
-            if incoming[i] == n - 1 and outgoing[i] == 0:
-                town_judge = i + 1
-                break
-        return town_judge
+        if len(trust) == 0 and n == 1: return 1
+        if len(trust) == 0: return -1
+        adj = defaultdict(list)
+        for i in range(1,n+1):
+            adj[i] = []
+        for lst in trust:
+            adj[lst[0]].append(lst[1])
+        
+        for i in range(1,n+1):
+            if adj[i] == [] and self.trusted(adj,i):
+                return i
+        return -1
