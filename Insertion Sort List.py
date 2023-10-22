6f53027e7f1f55ad021af766a8c6ddf49d9a71e5
unknown <kidusshun@gmail.com>
2023-10-22 21:06:37 +0300

    solutions

--- Insertion Sort List.py
+++ Insertion Sort List.py
@@ -1,14 +1,10 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    # [4,2,1,3]
-    # [2,4,1,3]
-    # [1,2,4,3]
-    # [1,2,3,4]
-    def insertionSortList(self, head):
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0,head)
         prev,curr =head, head.next
 
@@ -18,28 +14,10 @@ class Solution:
                 continue
             
             temp = dummy
-            while curr.val < temp.next.val:
+            while curr.val > temp.next.val:
                 temp = temp.next
             prev.next = curr.next
             curr.next = temp.next
             temp.next = curr
             curr = prev.next
         return dummy.next
-
-a = ListNode(
-    val=4,
-    next=ListNode(
-        val=2,
-        next=ListNode(
-            val=1,
-            next=ListNode(
-                val=3,
-                next=None
-            )
-        )
-    )
-)
-
-s = Solution()
-
-s.insertionSortList(a)
