b62a24b8e49d528f60414773d8b5fd2a0d614219
Kidus.H <95585233+kidusshun@users.noreply.github.com>
2024-01-15 15:06:34 +0300

    Added solution for Count Number of Nice Subarrays

--- leetcode solutions/count-number-of-nice-subarrays.py
+++ leetcode solutions/count-number-of-nice-subarrays.py
@@ -1,23 +1,13 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        odds = []
-        for i,num in enumerate(nums):
-            if num%2!=0:
-                odds.append(i)
-        if len(odds) ==0:
-            return 0
-        l,r = 0,k-1
-        ans = 0
-        while r<len(odds):
-            if l == 0:
-                left = odds[l]+1
-            else:
-                left = odds[l]-odds[l-1]
-            if r==len(odds)-1:
-                right = len(nums) - odds[r]
-            else:
-                right = odds[r+1] - odds[r]
-            ans+=right*left
-            r+=1
-            l+=1
-        return ans
+        dic = defaultdict(int)
+        num_odds,num_sub = 0,0
+        dic[0] = 1
+        for i in range(len(nums)):
+            if nums[i]%2 != 0:
+                num_odds+=1
+            num_sub += dic[num_odds-k]
+            dic[num_odds]+=1
+        return num_sub
+            
+
