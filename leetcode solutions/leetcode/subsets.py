404c91ec0f5a6ac6f9ac025e728811600b72b2f0
Kidus.H <95585233+kidusshun@users.noreply.github.com>
2024-02-27 14:38:37 +0300

    Add solution for Subsets

--- leetcode solutions/leetcode/subsets.py
+++ leetcode solutions/leetcode/subsets.py
@@ -1,20 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = [[]]
+        ans = []
         path = []
         seen = set()
         
         def backtrack(ind):
-            print(path)
+            ans.append(path[:])
             if ind > len(nums):
                 return
 
             for i in range(ind+1,len(nums)):
                 path.append(nums[i])
-                if tuple(path) not in seen:
-                    seen.add(tuple(path))
-                    ans.append(path[:])
-                    backtrack(i)
+                backtrack(i)
                 path.pop()
         backtrack(-1)
         return ans
